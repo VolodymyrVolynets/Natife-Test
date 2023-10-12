797a7c283811c52bcf3c15caa648d539b5cf8449
Volodymyr Volynets <volodymyr.volynets@student.griffith.ie>
2023-10-12 20:47:30 +0100

    Initial Commit

---
+++ Natife-Test/Natife_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Natife_TestApp.swift
+//  Natife-Test
+//
+//  Created by Vova on 12/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Natife_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
