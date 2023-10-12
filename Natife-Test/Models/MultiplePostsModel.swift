fe5e6ece38993edf76ba2989c940b5a1fe2f8deb
Volodymyr Volynets <volodymyr.volynets@student.griffith.ie>
2023-10-12 20:49:39 +0100

    Post Model Added

---
+++ Natife-Test/Models/MultiplePostsModel.swift
@@ -0,0 +1,12 @@
+//
+//  MultiplePostsModel.swift
+//  Natife-Test-UIKit
+//
+//  Created by Vova on 12/10/2023.
+//
+
+import Foundation
+
+struct MultiplePostsModel: Codable {
+    let posts: [PostModel]
+}
---
+++ Natife-Test/Models/PostModel.swift
@@ -0,0 +1,24 @@
+//
+//  PostModel.swift
+//  Natife-Test-UIKit
+//
+//  Created by Vova on 12/10/2023.
+//
+
+import Foundation
+
+struct PostModel: Codable {
+    let postId: Int
+    let date: Date
+    let title: String
+    let previewText: String
+    let likesCount: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case postId
+        case date = "timeshamp"
+        case title
+        case previewText = "preview_text"
+        case likesCount = "likes_count"
+    }
+}
